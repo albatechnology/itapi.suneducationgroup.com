0551655818e03e96d889cbf26fbecc033c3dad25
albatechnology <albaprogrammer2@gmail.com>
2022-10-26 09:24:31 +0700

    update untuk edit formpermintaa

--- routes/formPermintaan.routes.js
+++ routes/formPermintaan.routes.js
@@ -9,18 +9,27 @@ module.exports = (app) => {
   router.get("/", (req, res) => {
     formPermintaan.getAll(req, res);
   });
+
+  router.get("/:id", (req, res) => {
+    formPermintaan.getByFormPermintaanId(req, res);
+  });
+
   router.post("/", (req, res) => {
     formPermintaan.create(req, res);
   });
+
   router.post("/generatepdf", (req, res) => {
     //console.log(req.body);
     formPermintaan.generatePdf(req, res);
   });
-  router.put("/", (req, res) => {
+
+  router.put("/:id", (req, res) => {
     formPermintaan.update(req, res);
   });
+
   router.delete("/:id", (req, res) => {
     formPermintaan.delete(req, res);
   });
+
   app.use("/api/formpermintaan", router);
 };
