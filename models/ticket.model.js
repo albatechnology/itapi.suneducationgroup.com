63e4ec5ad4a43563303f6927f6785a74296d373e
albatechnology <albaprogrammer2@gmail.com>
2022-12-14 10:19:52 +0700

    model tiket

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
     },
     tanggal_pengajuan: {
-      type: Sequelize.DATE,
+      type: Sequelize.STRING,
     },
     alasan: {
       type: Sequelize.STRING,
